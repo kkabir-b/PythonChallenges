7b6c31f5d5e5cd2175dcb28a3d55a58a57768889
kkabir-b <107753204+kkabir-b@users.noreply.github.com>
2023-11-04 15:53:28 +0800

    Create AS37-RövarspråketCipher.py

---
+++ AS37-RövarspråketCipher.py
@@ -0,0 +1,10 @@
+def encode(word):
+  return_str=''
+  vowel=['a','e','i','o','u']
+  for i in word:
+    if i.lower() not in vowel and i!=' ':
+      return_str+=i+'o'+i
+    else:
+      return_str+=i
+  print(return_str)
+encode('Hello world')
