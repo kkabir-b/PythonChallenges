54a7208435f2298dad6f6822aab1e30be94c16e5
kkabir-b <107753204+kkabir-b@users.noreply.github.com>
2023-11-05 21:35:20 -0800

    Create AS39-XFactor.py

---
+++ AS39-XFactor.py
@@ -0,0 +1,22 @@
+import random
+lst=[[],[],[],[],[],[],[],[],[],[]]
+x=0
+for i in lst:
+  x+=1
+  for y in range(1,11):
+    i.append(('Song'+str(y)+' '+'Artist'+str(x)))
+
+last=0
+last_2=0
+for i in lst:
+  for y in i:
+    x=random.choice(lst)
+    m=random.choice(x)
+    while m[-1]==str(last) or m[-1]==str(last_2):
+      x=random.choice(lst)
+      m=random.choice(x)
+    print(m)
+    last_2=last
+    last=m[-1]
+      
+
