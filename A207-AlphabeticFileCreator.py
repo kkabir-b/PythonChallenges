33e82c51ccb31aacae66faed45aa69a81b973d4c
kkabir-b <107753204+kkabir-b@users.noreply.github.com>
2023-11-08 18:41:06 +0800

    Create A207-AlphabeticFileCreator.py

---
+++ A207-AlphabeticFileCreator.py
@@ -0,0 +1,8 @@
+x=input('Enter the list of words(separate them with a comma)-->')
+lst=x.split(',')
+
+lst=sorted(lst)
+
+f=open('Alphabetically_sorted_words.txt','w')
+f.write((','.join([str(i) for i in lst])))
+f.close()
