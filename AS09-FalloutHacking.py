3e813c937326b1effa043736114e66cd9b37e53c
kkabir-b <107753204+kkabir-b@users.noreply.github.com>
2023-11-25 08:50:10 +0800

    Create AS09-FalloutHacking.py

---
+++ AS09-FalloutHacking.py
@@ -0,0 +1,23 @@
+import random
+lst=['ansdkn','ads]p[pLSNPCIJKLA','HELLO','ANSOPD{Q.','NUMBER','JkLNXLZZZ XJNCASAXMS MXCNSAA','NOPE','NJAOSDm','NADS:M oaJNK:M','ANOPDKO{XLA','WHY','LAJDSPA','MALES','BYE']
+lst_2=['HELLO','NUMBER','NOPE','WHY','MALES','BYE']
+for i in lst:
+  print(i)
+word=random.choice(lst_2) 
+c=0
+
+got=False
+while c<5 and got!=True:
+  c+=1
+  x=0
+  ask=input('What is the word?')
+  for i in word:
+    if i in ask:
+      x+=1
+  if ask==word:
+    print('You have gotten the word')
+    got=True
+  else:
+    print('The likeness is-->',x)
+
+
